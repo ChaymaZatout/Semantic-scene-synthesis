7ac23cd84bbed3c6cac0cbf5cecf92d3cafcfb6a
chim <zatout.chayma@gmail.com>
2021-01-30 19:02:16 +0100

    Main prepared.

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ Contact : github.com/ChaymaZatout
 Time    : 30/01/21 02:33 م
 Desc:
 """
+from PIL import Image
+
+if __name__ == '__main__':
+    pass
+    # Read depth image:
+
+    # Ground Detection
+
+    # Point cloud:
+
+    # Segmentation:
+
+    # Classification:
+
+    # Mapping:
